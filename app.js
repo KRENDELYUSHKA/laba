82f6d9c2a970a401c8ec7239f4aff5c2210ff379
Denis Novikov <novikovden4ik03@gmail.com>
2022-10-12 13:39:42 +0300

    laba
    laba

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var input=document.getElementById('input');
 var button=document.getElementById('button');
 var p=document.getElementById('p');
-var regexp = /\b((([1-9])|([1-9][0-9])|(1\d{2})|(2[0-5][0-5]))\.(\d|([1-9][0-9])|(1\d{2})|(2[0-5][0-5]))\.(\d|([1-9][0-9])|(1\d{2})|(2[0-5][0-5]))\.((2[0-5][0-5])|(1\d{2})|([1-9][0-9])|\d))\b/;
+var regexp = /\b((([1-9])|([1-9][0-9])|(1\d{2})|(2[0-5][0-5]))\.(\d|([1-9][0-9])|(1\d{2})|(2[0-5][0-5]))\.(\d|([1-9][0-9])|(1\d{2})|(2[0-5][0-5]))\.((2[0-5][0-5])|(1\d{2})|([0-9][1-9])|\d))\b/;
 button.addEventListener('click', function () {
     if(!regexp.test(input.value)){
         p.innerHTML="Не корректный IP-адрес";
